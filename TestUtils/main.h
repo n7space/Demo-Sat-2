8fee41bf7d9661e165fb3e2ef51fdd9fb3bc63af
jrachucki <jrachucki@n7space.com>
2022-02-07 11:01:41 +0100

    main.h refactored

--- TestUtils/main.h
+++ TestUtils/main.h
@@ -23,65 +23,61 @@
  */
 
 #ifdef __cplusplus
-extern "C"
-{
+extern "C" {
 #endif
 
 #include "FreeRTOS.h"
 #include "task.h"
 
-#include <Init/Init.h>
 #include <Hal/Hal.h>
-
-    // #include <PioHwas/PioHwas.h>
+#include <Init/Init.h>
 
 #include <assert.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-    void vApplicationMallocFailedHook();
-    void vApplicationStackOverflowHook(TaskHandle_t pxTask, char* pcTaskName);
-    void vApplicationIdleHook();
-    void vApplicationTickHook();
+void vApplicationMallocFailedHook();
+void vApplicationStackOverflowHook(TaskHandle_t pxTask, char *pcTaskName);
+void vApplicationIdleHook();
+void vApplicationTickHook();
 
-    typedef struct __attribute__((packed)) ContextStateFrame
-    {
-        uint32_t r0;
-        uint32_t r1;
-        uint32_t r2;
-        uint32_t r3;
-        uint32_t r12;
-        uint32_t lr;
-        uint32_t return_address;
-        uint32_t xpsr;
-    } sContextStateFrame;
+typedef struct __attribute__((packed)) ContextStateFrame {
+  uint32_t r0;
+  uint32_t r1;
+  uint32_t r2;
+  uint32_t r3;
+  uint32_t r12;
+  uint32_t lr;
+  uint32_t return_address;
+  uint32_t xpsr;
+} sContextStateFrame;
 
 // NOTE: If you are using CMSIS, the registers can also be
 // accessed through CoreDebug->DHCSR & CoreDebug_DHCSR_C_DEBUGEN_Msk
-#define HALT_IF_DEBUGGING()                                                                                            \
-    do {                                                                                                               \
-        if((*(volatile uint32_t*)0xE000EDF0) & (1 << 0)) {                                                             \
-            __asm("bkpt 1");                                                                                           \
-        }                                                                                                              \
-    } while(0)
+#define HALT_IF_DEBUGGING()                                                    \
+  do {                                                                         \
+    if ((*(volatile uint32_t *)0xE000EDF0) & (1 << 0)) {                       \
+      __asm("bkpt 1");                                                         \
+    }                                                                          \
+  } while (0)
 
-    // Disable optimizations for this function so "frame" argument
-    // does not get optimized away
-    __attribute__((optimize("O0"))) void my_fault_handler_c(sContextStateFrame* frame)
-    {
-        // If and only if a debugger is attached, execute a breakpoint
-        // instruction so we can take a look at what triggered the fault
-        HALT_IF_DEBUGGING();
-    }
+// Disable optimizations for this function so "frame" argument
+// does not get optimized away
+__attribute__((optimize("O0"))) void
+my_fault_handler_c(sContextStateFrame *frame) {
+  // If and only if a debugger is attached, execute a breakpoint
+  // instruction so we can take a look at what triggered the fault
+  HALT_IF_DEBUGGING();
+}
 
-#define HARDFAULT_HANDLING_ASM(_x)                                                                                     \
-    __asm volatile("tst lr, #4 \n"                                                                                     \
-                   "ite eq \n"                                                                                         \
-                   "mrseq r0, msp \n"                                                                                  \
-                   "mrsne r0, psp \n"                                                                                  \
-                   "b my_fault_handler_c \n")
+#define HARDFAULT_HANDLING_ASM(_x)                                             \
+  __asm volatile("tst lr, #4 \n"                                               \
+                 "ite eq \n"                                                   \
+                 "mrseq r0, msp \n"                                            \
+                 "mrsne r0, psp \n"                                            \
+                 "b my_fault_handler_c \n")
 
-    void HardFault_Handler(void) { HARDFAULT_HANDLING_ASM(); }
+void HardFault_Handler(void) { HARDFAULT_HANDLING_ASM(); }
 #ifdef __cplusplus
 }
 #endif
@@ -89,40 +85,27 @@ extern "C"
 #include <CppUTest/CommandLineTestRunner.h>
 #include <CppUTest/TestHarness.h>
 
-int
-main()
-{
-    Init_setup_hardware();
+int main() {
+  Init_setup_hardware();
 
-    char** av{};
-    const int r = CommandLineTestRunner::RunAllTests(0, av);
+  char **av{};
+  const int r = CommandLineTestRunner::RunAllTests(0, av);
 
-    return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
 
-void
-vApplicationMallocFailedHook()
-{
-    configASSERT(0);
+void vApplicationMallocFailedHook() { configASSERT(0); }
+
+void vApplicationStackOverflowHook(TaskHandle_t pxTask, char *pcTaskName) {
+  (void)pxTask;
+  (void)pcTaskName;
+
+  configASSERT(0);
 }
 
-void
-vApplicationStackOverflowHook(TaskHandle_t pxTask, char* pcTaskName)
-{
-    (void)pxTask;
-    (void)pcTaskName;
-
-    configASSERT(0);
+void vApplicationIdleHook() {
+  volatile size_t xFreeHeapSpace = xPortGetFreeHeapSize();
+  (void)xFreeHeapSpace;
 }
 
-void
-vApplicationIdleHook()
-{
-    volatile size_t xFreeHeapSpace = xPortGetFreeHeapSize();
-    (void)xFreeHeapSpace;
-}
-
-void
-vApplicationTickHook()
-{
-}
+void vApplicationTickHook() {}
