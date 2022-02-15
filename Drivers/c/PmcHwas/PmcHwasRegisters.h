e268118528b36675b9cb60100deffb56527af114
jrachucki <jrachucki@n7space.com>
2022-02-15 17:24:04 +0100

    Definitions needed to init afec added to PmcHwasRegister

--- Drivers/c/PmcHwas/PmcHwasRegisters.h
+++ Drivers/c/PmcHwas/PmcHwasRegisters.h
@@ -29,18 +29,37 @@
 #define PMC_PCDR0_OFFSET (asn1SccSourceAddress)0x0014
 #define PMC_PCSR0_OFFSET (asn1SccSourceAddress)0x0018
 
+#define PMC_PCER1_OFFSET (asn1SccSourceAddress)0x0100
+#define PMC_PCDR1_OFFSET (asn1SccSourceAddress)0x0104
+#define PMC_PCSR1_OFFSET (asn1SccSourceAddress)0x0108
+
 #define PMC_PCxR0_PID10_PIOA_POSITION 10u
 #define PMC_PCxR0_PID11_PIOB_POSITION 11u
 #define PMC_PCxR0_PID12_PIOC_POSITION 12u
 #define PMC_PCxR0_PID16_PIOD_POSITION 16u
 #define PMC_PCxR0_PID17_PIOE_POSITION 17u
+#define PMC_PCxR0_PID29_AFEC0_POSITION 29u
 
-#define PMC_PCxR0_PID10_PIOA_MASK (asn1SccWordMask)(0x1 << PMC_PCxR0_PID10_PIOA_POSITION)
-#define PMC_PCxR0_PID11_PIOB_MASK (asn1SccWordMask)(0x1 << PMC_PCxR0_PID11_PIOB_POSITION)
-#define PMC_PCxR0_PID12_PIOC_MASK (asn1SccWordMask)(0x1 << PMC_PCxR0_PID12_PIOC_POSITION)
-#define PMC_PCxR0_PID16_PIOD_MASK (asn1SccWordMask)(0x1 << PMC_PCxR0_PID16_PIOD_POSITION)
-#define PMC_PCxR0_PID17_PIOE_MASK (asn1SccWordMask)(0x1 << PMC_PCxR0_PID17_PIOE_POSITION)
+#define PMC_PCxR1_PID40_AFEC1_POSITION 8u
+
+#define PMC_PCxR0_PID10_PIOA_MASK                                              \
+  (asn1SccWordMask)(0x1 << PMC_PCxR0_PID10_PIOA_POSITION)
+#define PMC_PCxR0_PID11_PIOB_MASK                                              \
+  (asn1SccWordMask)(0x1 << PMC_PCxR0_PID11_PIOB_POSITION)
+#define PMC_PCxR0_PID12_PIOC_MASK                                              \
+  (asn1SccWordMask)(0x1 << PMC_PCxR0_PID12_PIOC_POSITION)
+#define PMC_PCxR0_PID16_PIOD_MASK                                              \
+  (asn1SccWordMask)(0x1 << PMC_PCxR0_PID16_PIOD_POSITION)
+#define PMC_PCxR0_PID17_PIOE_MASK                                              \
+  (asn1SccWordMask)(0x1 << PMC_PCxR0_PID17_PIOE_POSITION)
+#define PMC_PCxR0_PID29_AFEC0_MASK                                             \
+  (asn1SccWordMask)(0x1 << PMC_PCxR0_PID29_AFEC0_POSITION)
+#define PMC_PCxR1_PID40_AFEC1_MASK                                             \
+  (asn1SccWordMask)(0x1 << PMC_PCxR1_PID40_AFEC1_POSITION)
 
 #define PMC_PCER0 (asn1SccSourceAddress)(PMC_OFFSET + PMC_PCER0_OFFSET)
 #define PMC_PCDR0 (asn1SccSourceAddress)(PMC_OFFSET + PMC_PCDR0_OFFSET)
 #define PMC_PCSR0 (asn1SccSourceAddress)(PMC_OFFSET + PMC_PCSR0_OFFSET)
+#define PMC_PCER1 (asn1SccSourceAddress)(PMC_OFFSET + PMC_PCER1_OFFSET)
+#define PMC_PCDR1 (asn1SccSourceAddress)(PMC_OFFSET + PMC_PCDR1_OFFSET)
+#define PMC_PCSR1 (asn1SccSourceAddress)(PMC_OFFSET + PMC_PCSR1_OFFSET)
