06c7f7501692d09c514d7ae3fbe6a3fa635eacc5
jrachucki-n7s <jrachucki@n7space.com>
2022-02-17 20:46:11 +0100

    Needed register definitions added

--- Drivers/c/UartHwas/UartHwasRegisters.h
+++ Drivers/c/UartHwas/UartHwasRegisters.h
@@ -20,3 +20,42 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+/// \brief Uart register definitions
+#pragma once
+
+#include "hwas/hwas.h"
+
+#define UART_HWAS_0_OFFSET (asn1SccSourceAddress)0x400E0800
+#define UART_HWAS_1_OFFSET (asn1SccSourceAddress)0x400E0A00
+#define UART_HWAS_2_OFFSET (asn1SccSourceAddress)0x400E1A00
+#define UART_HWAS_3_OFFSET (asn1SccSourceAddress)0x400E1C00
+#define UART_HWAS_4_OFFSET (asn1SccSourceAddress)0x400E1E00
+
+#define UART_HWAS_CR_OFFSET (asn1SccSourceAddress)0x00
+#define UART_HWAS_MR_OFFSET (asn1SccSourceAddress)0x04
+#define UART_HWAS_IER_OFFSET (asn1SccSourceAddress)0x08
+#define UART_HWAS_IDR_OFFSET (asn1SccSourceAddress)0x0C
+#define UART_HWAS_IMR_OFFSET (asn1SccSourceAddress)0x10
+#define UART_HWAS_IMR_OFFSET (asn1SccSourceAddress)0x14
+#define UART_HWAS_RHR_OFFSET (asn1SccSourceAddress)0x18
+#define UART_HWAS_THR_OFFSET (asn1SccSourceAddress)0x1C
+#define UART_HWAS_BRFR_OFFSET (asn1SccSourceAddress)0x20
+
+#define UART_HWAS_CR_RXEN_MASK (asn1SccWordMask)0x00000010
+#define UART_HWAS_CR_TXEN_MASK (asn1SccWordMask)0x00000040
+
+#define UART_HWAS_MR_NO_PARITY_MASK (asn1SccWordMask)0x00000E00
+#define UART_HWAS_MR_CHMODE_LOCAL_LOOPBACK_MASK (asn1SccWordMask)0x00008000
+
+#define UART_HWAS_IxR_RXRDY_MASK (asn1SccWordMask)0x00000001
+#define UART_HWAS_IxR_TXRDY_MASK (asn1SccWordMask)0x00000002
+#define UART_HWAS_IxR_OVRE_MASK (asn1SccWordMask)0x00000020
+#define UART_HWAS_IxR_FRAME_MASK (asn1SccWordMask)0x00000040
+#define UART_HWAS_IxR_PARE_MASK (asn1SccWordMask)0x00000080
+#define UART_HWAS_IxR_TXEMPTY_MASK (asn1SccWordMask)0x00000200
+
+#define UART_HWAS_BRGR_115200_MASK (asn1SccWordMask)0x00000028 //< Assuming peripheral clock is 75 MHz
+#define UART_HWAS_BRGR_9600_MASK (asn1SccWordMask)0x000001E8 //< Assuming peripheral clock is 75 MHz
+
+
