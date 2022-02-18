58ee4f74d917de14a4451c1e1bea38bfe818c0bc
jrachucki-n7s <jrachucki@n7space.com>
2022-02-18 09:55:31 +0100

    UartHwasInit implemented

--- Drivers/c/UartHwas/UartHwas.c
+++ Drivers/c/UartHwas/UartHwas.c
@@ -20,3 +20,90 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+#include <UartHwas/UartHwas.h>
+#include <UartHwas/UartHwasRegisters.h>
+
+#include <PmcHwas/PmcHwasRegisters.h>
+#include <RegisterHwas/RegisterHwas.h>
+
+static inline asn1SccSourceAddress
+UartHwas_init_get_uart_register(UartHwas_Id id) {
+  switch (id) {
+  case UartHwas_Id_0:
+    return UART_HWAS_0_OFFSET;
+  case UartHwas_Id_1:
+    return UART_HWAS_1_OFFSET;
+  case UartHwas_Id_2:
+    return UART_HWAS_2_OFFSET;
+  case UartHwas_Id_3:
+    return UART_HWAS_3_OFFSET;
+  case UartHwas_Id_4:
+    return UART_HWAS_4_OFFSET;
+  }
+}
+
+static inline void UartHwas_init_pmc_init(UartHwas_Id id) {
+  switch (id) {
+  case UartHwas_Id_0:
+    if (!(Register_get_bits(PMC_PCSR0, PMC_PCxR0_PID7_UART0_MASK)))
+      Register_set_bits(PMC_PCER0, (asn1SccWord)PMC_PCxR0_PID7_UART0_MASK);
+    break;
+  case UartHwas_Id_1:
+    if (!(Register_get_bits(PMC_PCSR0, PMC_PCxR0_PID8_UART1_MASK)))
+      Register_set_bits(PMC_PCER0, (asn1SccWord)PMC_PCxR0_PID8_UART1_MASK);
+    break;
+  case UartHwas_Id_2:
+    if (!(Register_get_bits(PMC_PCSR1, PMC_PCxR1_PID44_UART2_MASK)))
+      Register_set_bits(PMC_PCER1, (asn1SccWord)PMC_PCxR1_PID44_UART2_MASK);
+    break;
+  case UartHwas_Id_3:
+    if (!(Register_get_bits(PMC_PCSR1, PMC_PCxR1_PID45_UART3_MASK)))
+      Register_set_bits(PMC_PCER1, (asn1SccWord)PMC_PCxR1_PID45_UART3_MASK);
+    break;
+  case UartHwas_Id_4:
+    if (!(Register_get_bits(PMC_PCSR1, PMC_PCxR1_PID46_UART4_MASK)))
+      Register_set_bits(PMC_PCER1, (asn1SccWord)PMC_PCxR1_PID46_UART4_MASK);
+    break;
+  }
+}
+
+static inline void UartHwas_init_cr_init(const UartHwas *const uart) {
+  Register_set_bits(uart->uartAddress + UART_HWAS_CR_OFFSET,
+                    UART_HWAS_CR_TXEN_MASK | UART_HWAS_CR_RXEN_MASK);
+}
+
+static inline void UartHwas_init_mr_init(const UartHwas *const uart) {
+  Register_reset_bits(uart->uartAddress + UART_HWAS_MR_OFFSET,
+                      UART_HWAS_MR_CHMODE_MASK);
+  Register_set_bits(uart->uartAddress + UART_HWAS_MR_OFFSET,
+                    UART_HWAS_MR_NO_PARITY_MASK);
+}
+static inline void
+UartHwas_init_brgr_init(const UartHwas *const uart,
+                        const UartHwas_Config *const config) {
+  Register_reset_bits(uart->uartAddress + UART_HWAS_BRGR_OFFSET,
+                      UART_HWAS_BRGR_CD_MASK);
+  switch (config->baudrate) {
+  case UartHwas_Baudrate_9600:
+    Register_set_bits(uart->uartAddress + UART_HWAS_BRGR_OFFSET,
+                      UART_HWAS_BRGR_CD_9600_VALUE);
+    break;
+  case UartHwas_Baudrate_115200:
+    Register_set_bits(uart->uartAddress + UART_HWAS_BRGR_OFFSET,
+                      UART_HWAS_BRGR_CD_115200_VALUE);
+    break;
+  }
+}
+
+void UartHwas_init(UartHwas *const uart, const UartHwas_Config *const config) {
+  uart->uartAddress = UartHwas_init_get_uart_register(config->id);
+  UartHwas_init_pmc_init(config->id);
+  UartHwas_init_cr_init(uart);
+  UartHwas_init_mr_init(uart);
+  UartHwas_init_brgr_init(uart, config);
+}
+
+void UartHwas_rx(UartHwas *const uart) {}
+
+void UartHwas_tx(UartHwas *const uart) {}
