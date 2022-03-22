ce29aa3aec722ca28ba374205a6c32c9709f79c6
jrachucki-n7s <jrachucki@n7space.com>
2022-03-22 10:16:31 +0100

    main.c skeleton added

---
+++ Drivers/seds/PioHwas/test/main.c
@@ -0,0 +1,31 @@
+/**@file
+ * This file is part of the ARM BSP for the Test Environment.
+ *
+ * @copyright 2020-2022 N7 Space Sp. z o.o.
+ *
+ * Test Environment was developed under a programme of,
+ * and funded by, the European Space Agency (the "ESA").
+ *
+ *
+ * Licensed under the ESA Public License (ESA-PL) Permissive,
+ * Version 2.3 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://essr.esa.int/license/list
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "dataview-uniq.h"
+
+void piohwasdriver_PI_PioHwas_InitPin_Pi(asn1SccPioHwas *pio,
+                                         asn1SccPioHwasPinConfig *config);
+void piohwasdriver_PI_PioHwas_ResetPin_Pi(asn1SccPioHwas *pio);
+void piohwasdriver_PI_PioHwas_SetPin_Pi(asn1SccPioHwas *pio);
+
+int main(void) { return 0; }
