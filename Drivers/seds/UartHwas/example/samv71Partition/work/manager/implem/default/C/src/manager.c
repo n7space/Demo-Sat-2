f7e0edabd139b71866360662b6274386a3199ce6
jrachucki-n7s <jrachucki@n7space.com>
2022-04-21 15:30:31 +0200

    Samv71 partition part of the example added

--- Drivers/seds/UartHwas/example/samv71Partition/work/manager/implem/default/C/src/manager.c
+++ Drivers/seds/UartHwas/example/samv71Partition/work/manager/implem/default/C/src/manager.c
@@ -8,14 +8,27 @@
  */
 
 #include "manager.h"
+#include <string.h>
+
 static bool wasInitialized = false;
-static char * msg = {"Hello\n\r"};
+static char * msg = {"MSG Received\n\r"};
 
 static asn1SccUartHwas uart;
 static asn1SccUartHwasConfig config = {.mInstance = asn1SccuartHwas_Instance_4, .mBaudrate = asn1SccuartHwas_Baudrate9600};
 
-#define MSG_LEN 7
+static const char const * RX_MSG {"Hello\n\r"};
+
+#define MSG_LEN 14
+#define RX_BUFFER_LENGTH 7
+
+#define START_BYTE (uint8_t)0x00
+#define STOP_BYTE (uint8_t)0xFF
+
+static char rxBuffer[RX_BUFFER_LENGTH];
 static int sentBytes = 0;
+static int receivedBytes =0;
+
+static bool msgDetected;
 
 void manager_startup(void)
 {
@@ -30,17 +43,47 @@ void manager_PI_SendData( void )
         manager_RI_UartHwas_InitUartCmd_Pi(&uart, &config);
         wasInitialized = true;
     }
-    else {
-        sentBytes = 0;
-        asn1SccSendByteAsyncCmd_Type sendByteStructure = {.uart = uart, .byteToSend = msg[sentBytes]};
-        manager_RI_UartHwas_SendByteAsyncCmd_Pi(&sendByteStructure);
-    }
 }
 
 void manager_PI_UartHwas_ReadByteAsyncCmd_Ri
       (const asn1SccReadByteAsyncCmd_Type1 *IN_inputparam)
 
 {
+    if(IN_inputparam->byteToRead == START_BYTE)
+    {
+        msgDetected = true;
+    }else if(IN_inputparam->byteToRead == STOP_BYTE)
+    {
+        if(strcmp(RX_MSG, rxBuffer))
+        {
+            while(true)
+            {
+                //Received msg is corrupted
+            }
+        }else {
+            sentBytes = 0;
+            asn1SccSendByteAsyncCmd_Type sendByteStructure = {.uart = uart, .byteToSend = msg[sentBytes]};
+            manager_RI_UartHwas_SendByteAsyncCmd_Pi(&sendByteStructure);
+            msgDetected = false;
+            receivedBytes = 0;
+        }
+    }else if(msgDetected == true)
+    {
+        if(receivedBytes < RX_BUFFER_LENGTH)
+        {
+            rxBuffer[receivedBytes] = IN_inputparam->byteToRead;
+            receivedBytes++;
+        }
+        else {
+            while(true)
+            {
+                //Error no STOP byte
+            }
+        }
+    }
+
+
+
    // Write your code here
 }
 
