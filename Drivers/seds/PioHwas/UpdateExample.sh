932de69b3b9db82f1a6b6e029c6a0ace5f0caf52
jrachucki-n7s <jrachucki@n7space.com>
2022-05-02 12:40:58 +0200

    Not needed bash scripts deleted

--- Drivers/seds/PioHwas/UpdateExample.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mv hwasBckp.c example/work/hwas/implem/default/C/src/hwas.c
-mv managerBckp.c example/work/manager/C/src/manager.c
-cp ~/Documents/sedsDrivers/pio/output/piohwasdriver.pr example/work/piohwasdriver/SDL/src/piohwasdriver.pr
-
-rm example/skeletons.pro
-
-spacecreator.AppImage example/example.pro
